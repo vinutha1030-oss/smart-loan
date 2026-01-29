372018369f3dac4427aaa4ddc833048e4eab9f7a
vinutha1030-oss <vinuthavinu1030@gmail.com>
2026-01-29 09:22:45 +0530

    Add files via upload

---
+++ java3.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class java3 {
+	public static void main(String[]args) {
+		int a=40;
+		int b=20;
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a);
+		System.out.println(b);
+	
+		
+		}
+
+}
