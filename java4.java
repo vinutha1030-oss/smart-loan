b021a338096b90d4630434d176ab5fc9240b8d21
vinutha1030-oss <vinuthavinu1030@gmail.com>
2026-01-29 09:24:12 +0530

    Add files via upload

---
+++ java4.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class java4 {
+	public static void main(String[]args) {
+		int a=10;
+	    int b=20;
+		
+	
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+				
+	}
+
+}
