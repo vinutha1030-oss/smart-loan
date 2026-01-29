7e11a297a95b8c04da48447fb54fab97efe59480
vinutha1030-oss <vinuthavinu1030@gmail.com>
2026-01-29 09:20:31 +0530

    Add files via upload

---
+++ code1.java
@@ -0,0 +1,17 @@
+package code1;
+
+import java.util.Scanner;
+
+public class code1 {
+	    public static void main(String[]args) {
+	    	int a = 10;
+	    	char b = 'a';
+	    	double c = 0.12;
+	    	boolean d = true;
+	  System.out.println(a);
+      System.out.println(b);
+	  System.out.println(c);
+	  System.out.println(d);
+		}
+	
+}
